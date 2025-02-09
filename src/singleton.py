ebac42c41b30e6fe595348104c0c90f83b54e889
arthurym <arthuryamamoto25@gmail.com>
2025-02-09 14:11:50 -0300

    copying stuff

--- src/singleton.py
+++
@@ -1,6 +0,0 @@
-# REF: https://www.geeksforgeeks.org/singleton-pattern-in-python-a-complete-guide/
-class Singleton(object):
-    def __new__(cls):
-        if not hasattr(cls, 'instance'):
-            cls.instance = super(Singleton, cls).__new__(cls)
-        return cls.instance
